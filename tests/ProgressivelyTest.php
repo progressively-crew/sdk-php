d6d6ceb6847e404071c05f4988fe49b38b87545c
Nicolas Eeckeloo <neeckeloo@gmail.com>
2023-01-07 12:29:16 +0100

    chore(tests): add PHPUnit config file

--- tests/ProgressivelyTest.php
+++ tests/ProgressivelyTest.php
@@ -3,15 +3,10 @@
 namespace Progressively;
 
 use PHPUnit\Framework\TestCase;
-use Progressively\Progressively;
 use function PHPUnit\Framework\assertEquals;
 
-require __DIR__ . "/../src/Progressively.php";
-require __DIR__ . "/../src/Http.php";
-
-class ProgressivelyTest extends TestCase
+final class ProgressivelyTest extends TestCase
 {
-
     public function testRetrievingAnExistingAndActivatedFlag()
     {
         // Arrange
